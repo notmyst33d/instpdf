d9494e4962529c6b34167a2312d7ea9d50a4289f
Myst33d <notmyst33d@gmail.com>
2025-12-24 23:26:22 +0600

    Use normal Node

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs-slim_24
+            nodejs_24
             ghostscript
             imagemagick
           ];
